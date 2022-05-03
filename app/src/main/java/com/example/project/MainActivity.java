c005fa3ee1235a05e7214c6302c4562b5ba42062
d21emijo <d21emijo@student.his.se>
2022-05-03 14:21:36 +0200

    +Shared preferences

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewName;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+
+
+        textViewName = findViewById(R.id.name);
+        preferences = getSharedPreferences("preferences",MODE_PRIVATE);
+        /*
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("name", "emil");
+        editor.apply();
+        */
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        String name = preferences.getString("name", "NO NAME FOUND");
+        textViewName.setText(name);
+
+    }
 }
